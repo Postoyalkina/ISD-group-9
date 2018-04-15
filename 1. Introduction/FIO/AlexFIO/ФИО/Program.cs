1fb8931ee21157340f0bc5acf58b97be01b27877
Misha Krygin <Noderoi64@gmain.com>
2018-04-15 18:33:29 +0300

    add FIO alex

---
+++ 1. Introduction/FIO/AlexFIO/ФИО/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ФИО
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+{0}+",new string('-',31));
+                Console.WriteLine("|Круглый Александр Станиславович|");
+            Console.WriteLine("+{0}+", new string('-', 31));
+            Console.ReadLine();
+        }
+    }
+}
