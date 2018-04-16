bd30ca51ac46385015f579b465ebb5a69e3b8d66
Postoyalkina <asya.9.9.9.m@gmail.com>
2018-04-16 16:13:50 +0300

    add task 3

---
+++ 2.Classes/3/3/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Author
+    {
+        public string AuthorText { get; set; }
+        public void Show()
+        {
+            Console.WriteLine("Author: " + AuthorText);
+        }
+    }
+}
---
+++ 2.Classes/3/3/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Book
+    {
+        public Title BookTitle { get; set; }
+        public Author BookAuthor { get; set; }
+        public Content BookContent { get; set; }
+
+        public Book(string title, string author, string content)
+        {
+            BookTitle = new Title
+            {
+                TitleText = title
+            };
+            BookAuthor = new Author
+            {
+                AuthorText = author
+            };
+            BookContent = new Content
+            {
+                ContentText = content
+            };
+        }
+    }
+}
