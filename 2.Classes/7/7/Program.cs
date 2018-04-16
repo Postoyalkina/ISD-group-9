3eadef604a051b124a07bc3a565b78a9afb1f74b
Postoyalkina <asya.9.9.9.m@gmail.com>
2018-04-16 16:41:12 +0300

    add task 7

---
+++ 2.Classes/7/7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Postoyalkina", "Anastasia", "Director", 15);
+            employee.Show();
+            Console.ReadKey();
+        }
+    }
+}
