52369bfa53a090bd4981ddb848562febea52a5da
eugenezhornyak <eugenezhornyak@gail.com>
2018-04-20 17:50:10 +0300

    Inheritance
    3_1 Done

---
+++ 3.Inheritance/1/3_1/Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_1
+{
+    class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.WriteLine(value);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    class GreenPrinter:Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            base.Print(value);
+        }
+    }
+    class BluePrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            base.Print(value);
+        }
+    }
+}
---
+++ 3.Inheritance/1/3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new GreenPrinter();
+            printer.Print("Green grass");
+            printer = new BluePrinter();
+            printer.Print("Blue sky");
+            printer = new Printer();
+            printer.Print("Base functional");
+            Console.ReadKey();
+        }
+    }
+}
