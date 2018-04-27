d8612c9b2b5d6d0365aef030bd65d0f2dbe02711
Postoyalkina <asya.9.9.9.m@gmail.com>
2018-04-27 21:18:38 +0300

    Task 6_1

---
+++ 6.Struct/6_1/6_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_1
+{
+    struct Notebook
+    {
+        /* model -> модель блокноте
+         * manufacturer -> производитель
+         * price -> цена
+         */
+        private string model;
+        private string manufacturer;
+        private double price;
+
+        public Notebook(string model, string manufacturer, double price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        //вывод информации о блокноте (модель, производитель, цена)
+        public void Show()
+        {
+            Console.WriteLine("Model: " + model + "\nManufacturer: " + manufacturer + "\nPrice: " + price);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter notebook model");
+            string model = Console.ReadLine();
+            Console.WriteLine("Enter notebook manufacturer");
+            string manufacturer = Console.ReadLine();
+            Console.WriteLine("Enter notebook price");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Notebook notebook = new Notebook(model, manufacturer, price);
+            notebook.Show();
+            Console.ReadKey();
+        }
+    }
+}
