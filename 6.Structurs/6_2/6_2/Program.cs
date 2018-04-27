ff6d81c7a73a6628852873e47c648d84169a3cfd
= <noderoid64@gmail.com>
2018-04-27 19:26:11 +0300

    Tasks 6(misha) 2 and 3 (Eugene)

---
+++ 6.Structurs/6_2/6_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train[] trains = new Train[8];
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Введите пункт назначения для рейса: " + (i + 1).ToString());
+                trains[i].nameOfDestination = Console.ReadLine();
+                Console.WriteLine("Введите номер поезда для рейса: " + (i + 1).ToString());
+                while (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Введите номер поезда для рейса: " + (i + 1).ToString());
+                    trains[i].numberOfTrain = number;
+                }
+                while (!int.TryParse(Console.ReadLine(), out int hour) && hour < 24)
+                {
+                    Console.WriteLine("Введите час для рейса: " + (i + 1).ToString());
+                    trains[i].departureTime.AddHours(Convert.ToDouble(hour));
+                }
+            }
+            Console.WriteLine(trains[0].departureTime.ToString());
+            Console.ReadKey();
+        }
+    }
+    struct Train
+    {
+        public string nameOfDestination;
+        public int numberOfTrain;
+        public DateTime departureTime;
+    }
+}
---
+++ 6.Structurs/6_3/6_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass
+            {
+                change = "не изменено"
+            };
+            ClassTaker(myClass);
+            MyStruct myStruct = new MyStruct
+            {
+                change = "не изменено"
+            };
+            StruktTaker(myStruct);
+            Console.WriteLine("Class: " + myClass.change);
+            Console.WriteLine("Struct: " + myStruct.change);
+            Console.ReadKey();
+        }
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+    }
+    class MyClass
+    {
+        public string change;
+    }
+    struct MyStruct
+    {
+        public string change;
+    }
+}
