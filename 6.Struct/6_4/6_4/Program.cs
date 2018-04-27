44e2ae7c4368838cfe75a5748d7b7c47b34b4cf1
Postoyalkina <asya.9.9.9.m@gmail.com>
2018-04-27 21:46:12 +0300

    Task 6_4

---
+++ 6.Struct/6_4/6_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day, month, year;
+            Console.WriteLine("Введите дату своего рождения\nДень: ");
+            day = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Месяц: ");
+            month = Int32.Parse(Console.ReadLine());
+
+            if ((DateTime.Now.Month > month) || (DateTime.Now.Month == month && DateTime.Now.Day > day))
+            {
+                year = DateTime.Now.Year + 1;
+            }
+            else
+            {
+                year = DateTime.Now.Year;
+            }
+
+            DateTime birthday = new DateTime(year, month, day);
+            TimeSpan left = birthday - DateTime.Now;
+            int days = (int)left.TotalDays;
+       
+            Console.WriteLine("Still to your birthday " + days + " days");
+
+            Console.ReadKey();
+        }
+    }
+}
