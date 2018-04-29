282eeb5cbd42c0229842c820a1f03114bed9154b
eugenezhornyak <eugenezhornyak@gail.com>
2018-04-29 21:20:36 +0300

    7_1 has just been finished

---
+++ 7.Delegats/7_1/7_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Average average = delegate(int dx ,int dy,int dz)
+            {
+                return Convert.ToDouble(dx + dy + dz) / 3;
+            };
+            Console.WriteLine("Введите первое число:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число:");
+            z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Round(average(x,y,z),2));
+            Console.ReadKey();
+        }
+        delegate double Average(int x, int y, int z);
+    }
+}
