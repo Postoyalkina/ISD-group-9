9d4829bd8ed728fefa7a0ea5a17cd6a0bcb8f023
Postoyalkina <asya.9.9.9.m@gmail.com>
2018-04-29 22:11:56 +0300

    Task 3

---
+++ 7.Delegats/7_3/7_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_3
+{
+    //тип делегата возвращающего int
+    public delegate int Delegates();
+    public delegate double MyDelegate(Delegates[] delegates);
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            //массив делегатов возвращающих int
+            Delegates[] delegates =
+            {
+                new Delegates(Method),
+                new Delegates(Method),
+                new Delegates(Method),
+                new Delegates(Method)
+            };           
+
+            //анонимный метод
+            MyDelegate myDelegate = delegate(Delegates[] array)
+            {
+                //получаем среднее значение массива делегатов, каждый делегат вызывает метод Method 
+                double avg = array.Average(a => a.Invoke());
+                return avg;
+            };
+
+            Console.WriteLine("Avarage: " + myDelegate(delegates));
+            Console.ReadKey();
+        }
+
+        static int Method()
+        {
+            int result = random.Next(0, 1001);
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
