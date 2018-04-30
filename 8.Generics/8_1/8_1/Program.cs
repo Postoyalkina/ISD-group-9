2879d3b5972e968ff5cc0c4a023b7c392e216a99
eugenezhornyak <eugenezhornyak@gail.com>
2018-04-30 10:34:01 +0300

    8_1

---
+++ 8.Generics/8_1/8_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyClass<Account>.FactoryMethod().SetId(225).id.ToString());
+            Console.ReadKey();
+        }
+        
+    }
+    class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            T r = new T();
+            return r;
+        }
+    }
+    class Account
+    {
+        public int id;
+
+        public Account SetId(int p)
+        {
+            id = p;
+            return this;
+        }
+    }
+}
