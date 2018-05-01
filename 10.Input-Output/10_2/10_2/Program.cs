d0e478e5e68e4af4277ef9b1c1f837ac5449a785
Postoyalkina <asya.9.9.9.m@gmail.com>
2018-05-01 13:29:20 +0300

    Task 2

---
+++ 10.Input-Output/10_2/10_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "txtFile.txt";
+            string text = "Hiiiiii!!!!!!!!";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            try
+            {
+                
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(text);
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+
+                using (StreamReader sw = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    Console.WriteLine(sw.ReadLine());
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
