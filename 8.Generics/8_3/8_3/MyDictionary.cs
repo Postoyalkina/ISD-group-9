8a0e0c9cace7d223af4cb4ffef0774b4ded61bc4
Alex <fraksion@ya.ru>
2018-05-01 18:54:51 +0300

    Task 3

---
+++ 8.Generics/8_3/8_3/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_3
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public MyDictionary(int size)
+        {
+            keys = new TKey[size];
+            values = new TValue[size];
+        }
+
+        public TValue this[TKey key]
+        { 
+            get
+            {   for (int i = 0; i < keys.Length; i++)
+                    if (key.Equals(keys[i])) return values[i];
+                    return default(TValue);
+            }
+        }
+
+        public int Count { get => values.Length; }
+
+        public void Add(TKey key, TValue value)
+        {
+            for (int i = 0; i < keys.Length; i++)
+                if (key.Equals(keys[i])) throw new Exception("Такой ключ уже существует");
+            TKey[] temp_of_keys = new TKey[keys.Length + 1];
+            TValue[] temp_of_values = new TValue[values.Length + 1];
+            Array.Copy(keys, temp_of_keys, keys.Length);
+            Array.Copy(values, temp_of_values, values.Length);
+            temp_of_keys[keys.Length] = key;
+            temp_of_values[values.Length] = value;
+            keys = temp_of_keys;
+            values = temp_of_values;
+        }
+
+    }
+}
---
+++ 8.Generics/8_3/8_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MyDictionary<int, int> dictionary = new MyDictionary<int, int>(5);
+                dictionary.Add(1, 10);
+                dictionary.Add(2, 20);
+                dictionary.Add(3, 30);
+
+                Console.WriteLine(dictionary[3]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
