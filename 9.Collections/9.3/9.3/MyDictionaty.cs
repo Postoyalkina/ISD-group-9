517165b2083576efd9a38f2fca2aeacf6f692101
Postoyalkina <asya.9.9.9.m@gmail.com>
2018-05-01 13:12:11 +0300

    Task 3

---
+++ 9.Collections/9.3/9.3/MyDictionaty.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._3
+{
+    class MyDictionaty<Tkey,Tvalue>
+    {
+        private Tkey[] keysArray;
+        private Tvalue[] valsArray;
+        public int Count { get => keysArray.Length; }
+
+        public MyDictionaty()
+        {
+            keysArray = new Tkey[0];
+            valsArray = new Tvalue[0];
+        }
+
+        public Tvalue this[Tkey key]
+        {
+            get
+            {
+                int ind = 0;
+                for (int i = 0; i < keysArray.Length; i++)
+                {
+                    if (key.Equals(keysArray[i]))
+                        ind = i;
+                }
+                return valsArray[ind];
+            }
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempK = new Tkey[keysArray.Length + 1];
+            Array.Copy(keysArray, tempK, keysArray.Length);
+            tempK[keysArray.Length] = key;
+            keysArray = tempK;
+
+            Tvalue[] tempV = new Tvalue[valsArray.Length + 1];
+            Array.Copy(valsArray, tempV, valsArray.Length);
+            tempV[valsArray.Length] = value;
+            valsArray = tempV;
+        }
+
+        public IEnumerator<KeyValuePair<Tkey, Tvalue>> GetEnumerator()
+        {
+            for (int i = 0; i < keysArray.Length; i++)
+                yield return new KeyValuePair<Tkey, Tvalue>(keysArray[i], valsArray[i]);
+        }
+
+    }
+}
---
+++ 9.Collections/9.3/9.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaty<string, string> dictionaty = new MyDictionaty<string, string>();
+            dictionaty.Add("Hello", "World");
+            dictionaty.Add("Hi", "Nastya");
+            foreach(KeyValuePair<string,string> kv in dictionaty)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
