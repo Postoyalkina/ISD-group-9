986faed424d86c3627ba14c0f5c4e811dc648c5e
= <noderoid64@gmail.com>
2018-05-02 20:16:30 +0300

    Tasks 8_7 and 8_5

---
+++ 8.Generics/8_5/8.5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarCollection<Car> T = new CarCollection<Car>();
+            T.AddCar(new Car("ford mustang",2017));
+            Console.WriteLine(T.Length);
+            T.RemoveAll();
+            Console.WriteLine(T.Length);
+            Console.ReadKey();
+        }
+    }
+
+    class CarCollection<T> where T: Car
+    {
+        List<T> Collection;
+        public CarCollection()
+        {
+            Collection = new List<T>();
+        }
+        public void AddCar(T C)
+        {            
+            Collection.Add(C);
+        }
+
+        public int Length
+        {
+            get {return Collection.Count; }
+        }
+        public T this[int i]
+        {
+            get
+            {
+                return Collection[i];
+            }
+            set
+            {
+                Collection[i] = value;
+            }
+        }
+        public void RemoveAll()
+        {
+            Collection.RemoveAll((T)=>true);
+        }
+
+    }
+    class Car
+    {
+        string name;
+        int year;
+        public Car(string name, int year)
+        {
+            this.name = name;
+            this.year = year;
+        }
+    }
+}
---
+++ 8.Generics/8_7/8.7/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList R = new ArrayList();
+            MyArrayList Mr = new MyArrayList();
+            
+            Console.ReadKey();
+
+        }
+    }
+
+    class MyArrayList : IEnumerable
+    {
+        List<object> Lis;
+
+        public MyArrayList(object[] A = null)
+        {
+            if (A != null)
+                Lis = new List<object>(A);
+            else
+                Lis = new List<object>();
+        }
+        public void remove(object value)
+        {
+            Lis.Remove(value);
+        }
+        public void Add(object value)
+        {
+            Lis.Add(value);
+        }
+        public void AddRange(ICollection T)
+        {
+            foreach (var item in T)
+            {
+                Lis.Add(item);
+            }
+        }
+        public void Reverse()
+        {
+            Lis.Reverse();
+        }
+
+        public object this [int i] 
+        {
+            get
+            {
+                return Lis[i];
+            }
+            set
+            {
+                Lis[i] = value;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Lis.GetEnumerator();
+        }
+    }
+}
+
+
