41ae47b03c9a73fa3148bb6ec827c98f2b38ffaa
= <noderoid64@gmail.com>
2018-05-04 21:59:45 +0300

    Task 8_4

---
+++ 9.Collections/9_4/9_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myList<string> ML = new myList<string>(new string[] { "Hello", "my", "name" , "is" , "human"});
+            string []A = ML.GetArray<string>();
+            foreach (var item in A)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+    static class myArrayExtention
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            T [] Array = new T[list.Count<T>()];
+            int i = 0;
+            foreach (T item in list)
+            {
+                Array[i] = item;
+                i++;
+            }
+            return Array;
+        }
+    }
+}
---
+++ 9.Collections/9_4/9_4/myList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_4
+{
+    class myList<T> : IEnumerable<T>
+    {
+        T[] Array;
+
+        #region constructors
+        public myList() { }
+        public myList(T[] N)
+        {
+            Array = N;
+        }
+        #endregion
+
+        public void Add(T value)
+        {
+            T[] array = new T[Array.Length + 1];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                array[i] = Array[i];
+            }
+            array[Array.Length] = value;
+            Array = array;
+        }
+
+        public T this[int i]
+        {
+            get { return Array[i]; }
+            set { Array[i] = value; }
+        }
+        public int Length
+        {
+            get { return Array.Length; }
+        }
+
+
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                yield return Array[i];
+            }
+        }
+       
+
+    }
+}
