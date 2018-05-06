5b0eb4447f688d0640de38910f14e815f2ffc7f2
eugenezhornyak <eugenezhornyak@gail.com>
2018-05-06 21:44:48 +0300

    10_1

---
+++ 10.Input-Output/10_1/10_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"C:\test");
+            string path = @"C:\test\Folder_";
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.CreateDirectory(path + i);
+            }
+            Console.WriteLine("Check C:");
+            Console.ReadKey();
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.Delete(path + i);
+            }
+            Directory.Delete(@"C:\test");
+            Console.WriteLine("And now)");
+            Console.ReadKey();
+        }
+    }
+}
